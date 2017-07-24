a17d936606ad02e703bc322f5d7e04f2b2439c3f
Pankesh <post4pankesh@gmail.com>
2017-07-24 21:08:07 +0530

    minor changes

--- 3.calc.py
+++ 3.calc.py
@@ -2,19 +2,21 @@ def solution(test_case):
     n = int(test_case.split()[0])
     
     b = int(test_case.split()[1])
-    
     a = 1
     max_a = n
     max_int = 0
     screen_b = 0
-    for ai in range(max_a,0,-b):
-            bi = (n-ai)//b
-            print("{}*{}".format(ai,bi))
-            screen_b = ai*bi
-            if screen_b > max_int:
-                max_int = screen_b
-                    
-    print(max_int)
+    if n>=1 and b<=100 :
+        for ai in range(max_a,0,-b):
+                bi = (n-ai)//b
+                
+                screen_b = ai*bi
+                if screen_b > max_int:
+                    max_int = screen_b
+                        
+        print(max_int)
+    else :
+        pass
 
 
 
@@ -30,4 +32,4 @@ for i in  range(t):
     test_cases.append(test_case)
     test_case = ""
 for i in range(t):
-solution(test_cases[i])
+    solution(test_cases[i])
